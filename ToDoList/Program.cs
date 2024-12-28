188eea5cad74026a55dfe8f6192fe65d525fd243
BANKPRO\bert_kang <bert_kang@bankpro.com.tw>
2024-12-28 20:18:36 +0800

    feat: Add SwaggerUI

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using ToDoList.Repositories.Interfaces;
 using ToDoList.Repositories;
 using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -21,13 +22,25 @@ builder.Services.AddDbContext<TodoDbContext>(options =>
 // Add services to the container.
 
 builder.Services.AddControllers();
+// 4. Swagger Configuration
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Todo List API",
+        Version = "v1",
+        Description = "A simple Todo List API"
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo List API v1"));
 }
 
 app.UseHttpsRedirection();
