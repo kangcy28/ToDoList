1ccf470993bfb6f01d32ac987cbb94c49655d21a
BANKPRO\bert_kang <bert_kang@bankpro.com.tw>
2024-12-28 15:32:09 +0800

    Add project files.

---
+++ ToDoList/Data/TodoDbContext.cs
@@ -0,0 +1,25 @@
+﻿// Data/TodoDbContext.cs
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+public class TodoDbContext : DbContext
+{
+    public TodoDbContext(DbContextOptions<TodoDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Todo> Todos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Todo>()
+            .Property(t => t.Title)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        modelBuilder.Entity<Todo>()
+            .Property(t => t.Description)
+            .HasMaxLength(500);
+    }
+}
---
+++ ToDoList/Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoList.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
