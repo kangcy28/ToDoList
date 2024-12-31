13b4057a59a7abacf606dd44ac1ac79d2d8a4a91
BANKPRO\bert_kang <bert_kang@bankpro.com.tw>
2024-12-31 20:15:02 +0800

    fix SwaggerEndpoint

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -108,12 +108,17 @@ var app = builder.Build();
 // Configure the HTTP request pipeline
 //if (app.Environment.IsDevelopment())
 //{
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoList API V1");
-    });
+//app.UseSwagger();
+//app.UseSwaggerUI(options =>
+//{
+//    options.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoList API V1");
+//});
 //}
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoList API V1");
+});
 
 app.UseHttpsRedirection();
 
